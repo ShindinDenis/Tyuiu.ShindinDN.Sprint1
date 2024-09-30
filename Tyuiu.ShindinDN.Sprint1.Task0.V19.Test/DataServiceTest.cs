5acf452f5ee0c5d9b3509dff83f36e4e9a5f839c
ShindinDenis <Tyuiu.ShindinDN@outlook.com>
2024-09-30 21:07:58 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.ShindinDN.Sprint1.Task0.V19.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+using Tyuiu.ShindinDN.Sprint1.Task0.V19.Lib;
+
+namespace Tyuiu.ShindinDN.Sprint1.Task0.V19.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(10, res);
+        }
+    }
+}
